0da9c03641f8bb33a17aed9c21122a629c4513d9
Arunima-26 <arunimasharma049@gmail.com>
2026-05-26 19:33:54 +0530

    Refactor AddEvent component for async submission
    Updated the event submission logic to use async/await and changed alert message.

--- AddEvent.js
+++ AddEvent.js
@@ -1,33 +1,29 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 function AddEvent() {
-  const [data, setData] = useState({
-    title: "", date: "", venue: "", description: ""
+  const [event, setEvent] = useState({
+    title: "",
+    date: "",
+    venue: "",
+    description: "",
   });
 
-  const submit = () => {
-    axios.post("http://localhost:5000/events", data)
-      .then(() => alert("Event Created"));
+  const handleSubmit = async () => {
+    await axios.post("http://localhost:5000/events", event);
+    alert("Event Added");
   };
 
   return (
     <div>
       <h2>Add Event</h2>
 
-      <input placeholder="Title"
-        onChange={e => setData({ ...data, title: e.target.value })} />
+      <input placeholder="Title" onChange={e=>setEvent({...event,title:e.target.value})}/>
+      <input placeholder="Date" onChange={e=>setEvent({...event,date:e.target.value})}/>
+      <input placeholder="Venue" onChange={e=>setEvent({...event,venue:e.target.value})}/>
+      <input placeholder="Description" onChange={e=>setEvent({...event,description:e.target.value})}/>
 
-      <input placeholder="Date"
-        onChange={e => setData({ ...data, date: e.target.value })} />
-
-      <input placeholder="Venue"
-        onChange={e => setData({ ...data, venue: e.target.value })} />
-
-      <input placeholder="Description"
-        onChange={e => setData({ ...data, description: e.target.value })} />
-
-      <button onClick={submit}>Create</button>
+      <button onClick={handleSubmit}>Add</button>
     </div>
   );
 }
